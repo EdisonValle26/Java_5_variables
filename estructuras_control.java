1ead16daa36b53c39b763120842fbf4bc16e10b5
EdisonValle26 <andresvalle128@gmail.com>
2024-09-21 20:18:49 -0500

    Estrucutras de control

---
+++ estructuras_control.java
@@ -0,0 +1,29 @@
+public class estructuras_control{
+    public static void main(String[] args){
+     int num1 = 10;
+
+     if(num1 > 0){
+        System.out.println("El numero es positivo");
+     }else if(num1 < 0 ){
+        System.out.println("El numero es negativo");
+     }else if(num1 == 0){
+        System.out.println("El numero es 0");
+     }else{
+        System.out.println("El numero no existe o no es un numero");
+     }
+
+     
+     for(int j = 0; j < 10; j++){
+        System.out.println("El numero es: " + j);
+     }
+
+     int i = 0;
+     while(i <= 10){
+        if(i % 2 == 0){
+            System.out.println("El valor es: " + i);
+        }
+        i++;
+    }
+        
+    }
+}
